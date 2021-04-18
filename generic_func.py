224e5e9c81fc9fc185e143b7486a63cac849b624
Arindam Samanta <samantoz@gmail.com>
2021-04-18 00:27:50 -0500

    added new from laptop

--- generic_func.py
+++ generic_func.py
@@ -1,8 +1,18 @@
-import sys
-
+import os
+# from sklearn import dataset
 
 def main():
-    getwd = !pwd
-
+    print("First Module's Name: {}".format(__name__))
+    
 if __name__ == '__main__':
     main()
+
+def get_filename(datatyp):
+    data_loc = os.getcwd()
+    if datatyp == 'raw':
+        file_location = data_loc + "/raw/"
+    elif datatyp == 'processed':
+        file_location = data_loc + "/processed/"
+    else:
+        return ("invalid path")
+    return file_location
---
+++ module2.py
@@ -0,0 +1,5 @@
+import generic_func as gf
+
+print("Second Module's Name: {}".format(__name__))
+
+print ("Filepath :" ,gf.get_filename("raw"))
